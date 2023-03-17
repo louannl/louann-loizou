563074a4163901a383b813e787997a52b0da2a6c
Louann Loizou <louann.loizou@gmail.com>
2023-03-17 13:12:14 +0000

    remove react-toastify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Fragment } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <Fragment>
       <Layout />
-      <ToastContainer />
     </Fragment>
   );
 }
